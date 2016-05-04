e2c00baea581c9bba69bcdb60a85592c4b44c003
Renan <renan.taizo@gmail.com>
2016-05-04 14:48:52 -0300

    Adicionei testes de funcionamento

--- src/processoservidor/ProcessoServidor.java
+++ src/processoservidor/ProcessoServidor.java
@@ -5,17 +5,39 @@
  */
 package processoservidor;
 
+import interfaces.InterfaceServidor;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
 /**
  *
  * @author a1144847
  */
 public class ProcessoServidor {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static void loadGUI() {
+        String MENU1 = "### PROCESSO SERVIDOR ###\n\n";
+        MENU1 += "1. Cadastrar nova oferta de Hospedagem\n";
+        MENU1 += "2. Cadastrar nova oferta de Vôo\n";
+        MENU1 += "3. Consultar ofertas de Hospedagem\n";
+        MENU1 += "4. Consultar ofertas de Vôo\n\n";
+        
+        String MENU2 = "### CADASTRO DE OFERTAS ###\n\n";
     }
+
+    ServerEngine serverEngine;
+    static Registry servidorNomes;
+    
+    public static void main(String[] args) throws RemoteException {
+        InterfaceServidor serverEngine = new ServerEngine(); //objeto remoto do servidor
+        servidorNomes = LocateRegistry.createRegistry(8888);
+        servidorNomes.rebind("ServerEngine", serverEngine);
+        
+        loadGUI();
+        
+    }
+
     
 }
